2d58c03c77c58b5a88174448d8e314f5f9810ba4
673110492 <rincedonfack5@gmail.com>
2025-05-20 20:37:38 +0100

    modif message

--- resources/views/front/layouts/partiels/header.blade.php
+++ resources/views/front/layouts/partiels/header.blade.php
@@ -52,28 +52,32 @@
             </div>
         </div>
     </nav>
-      <div class="hero-wrap ftco-degree-bg" style="background-image: url('assets1/images/bus3.jpeg');"
-        data-stellar-background-ratio="0.5">
-        <div class="overlay"></div>
-        <div class="container">
-            <div class="row no-gutters slider-text justify-content-start align-items-center justify-content-center">
-                <div class="col-lg-8 ftco-animate">
-                    <div class="text w-100 text-center mb-md-5 pb-md-5">
-                        <h1 class="mb-4">Fast &amp; Easy Way To Rent A Car</h1>
-                        <p style="font-size: 18px;">A small river named Duden flows by their place and supplies it with the
-                            necessary regelialia. It is a paradisematic country, in which roasted parts</p>
-                        <a href="https://vimeo.com/45830194"
-                            class="icon-wrap popup-vimeo d-flex align-items-center mt-4 justify-content-center">
-                            <div class="icon d-flex align-items-center justify-content-center">
-                                <span class="ion-ios-play"></span>
-                            </div>
-                            <div class="heading-title ml-5">
-                                <span>Easy steps for renting a car</span>
-                            </div>
-                        </a>
-                    </div>
+
+
+    <div class="hero-wrap ftco-degree-bg" style="background-image: url('assets1/images/bus3.jpeg');"
+    data-stellar-background-ratio="0.5">
+    <div class="overlay"></div>
+    <div class="container">
+        <div class="row no-gutters slider-text justify-content-start align-items-center justify-content-center">
+            <div class="col-lg-8 ftco-animate">
+                <div class="text w-100 text-center mb-md-5 pb-md-5">
+                    <h1 class="mb-4">Réservez vos billets en ligne facilement</h1>
+                    <p style="font-size: 18px;">
+                        Planifiez vos trajets en quelques clics. Réservez vos billets de voyage partout et à tout moment, sans vous déplacer. Simple, rapide et sécurisé.
+                    </p>
+                    <a href="https://vimeo.com/45830194"
+                        class="icon-wrap popup-vimeo d-flex align-items-center mt-4 justify-content-center">
+                        <div class="icon d-flex align-items-center justify-content-center">
+                            <span class="ion-ios-play"></span>
+                        </div>
+                        <div class="heading-title ml-5">
+                            <span>Découvrez comment réserver un billet en ligne</span>
+                        </div>
+                    </a>
                 </div>
             </div>
         </div>
     </div>
+</div>
+
     <!-- END nav -->
